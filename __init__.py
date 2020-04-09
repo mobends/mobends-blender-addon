5040e51b95113a4d12c55b6015f9330a2209c0f0
Iwo Plaza <iwoplaza@gmail.com>
2020-04-09 19:51:15 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,70 @@
+# <pep8 compliant>
+
+bl_info = {
+    "name": "Mo' Bends Exporter",
+    "author": "Iwo Plaza",
+    "version": (1, 0, 0),
+    "blender": (2, 81, 0),
+    "location": "File > Import-Export",
+    "description":
+        "Exports all animations in JSON",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Import-Export"}
+
+
+import bpy
+from bpy.props import (
+    BoolProperty,
+    FloatProperty,
+    StringProperty,
+    EnumProperty
+    )
+from bpy_extras.io_utils import (
+    ImportHelper,
+    ExportHelper
+    )
+import math
+import mathutils
+from . import export_mobends
+from mathutils import Matrix
+
+
+class ExportMoBends(bpy.types.Operator, ExportHelper):
+    """Export a Mo' Bends animation JSON file"""
+
+    bl_idname = "export_anim.mobends"
+    bl_label = "Export Mo' Bends"
+    bl_options = {'PRESET'}
+
+    filename_ext = ".json"
+
+    use_selection = BoolProperty(
+        name="Selection Only",
+        description="Export selected objects only",
+        default=False,
+        )
+
+    def execute(self, context):
+        return export_mobends.save(context, self.filepath, self.use_selection)
+    #end execute
+#end ExportMoBends
+
+
+def menu_func_export(self, context):
+    self.layout.operator(ExportMoBends.bl_idname, text="Mo' Bends (.json)")
+
+
+def register():
+    bpy.utils.register_class(ExportMoBends)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+
+
+def unregister():
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+    bpy.utils.unregister_class(ExportMoBends)
+
+
+if __name__ == "__main__":
+    register()
---
+++ export_mobends.py
@@ -0,0 +1,26 @@
+# <pep8 compliant>
+
+import os
+import mathutils
+import bpy
+import json
+from .data import create_data
+
+
+def save(context,
+         filepath,
+         export_selection_only=True
+         ):
+
+    # Exit edit mode before exporting, so current object states are exported properly.
+    if bpy.ops.object.mode_set.poll():
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+    # Fetching data from the context
+    data = create_data(context)
+
+    # Exporting the data as JSON
+    with open(filepath, 'w', encoding='utf8', newline='\n') as f:
+        f.write(json.dumps(data, sort_keys=True, indent=4))
+
+    return { 'FINISHED' }
